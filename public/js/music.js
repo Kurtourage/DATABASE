ac0dbc2998a8fbb088c79c334470c4a403c086cd
happydeigh <deighdeighg@gmail.com>
2024-01-21 17:17:57 +0800

    fix music

--- public/js/music.js
+++ public/js/music.js
@@ -1,5 +1,23 @@
-window.onload = function() {
-    var audio = document.getElementById("music-btn");
+var audio = document.getElementById("music-sound");
+window.onload = () => {
+    const transition_el = document.querySelector('.transition');
+    setTimeout(() => {
+        transition_el.classList.remove('is-active');
+    }, 1);
+    audio.volume = 0.5;
     audio.loop = true;
     audio.play();
-};
+}
+
+const music_btn = document.querySelector('.music-btn');
+music_btn.addEventListener("click", () => {
+    music_btn.classList.toggle('inactive');
+    if (music_btn.classList.contains('inactive')) {
+        audio.volume = 0;
+    } else {
+        // Set the desired volume when the 'inactive' class is not present
+        audio.volume = 0.5;
+    }
+});
+
+
