c55b7ec6a8cf158a932f9c58e1ad92c46d250770
happydeigh <deighdeighg@gmail.com>
2024-02-04 11:09:58 +0800

    fix leaderboard

--- public/js/classic.js
+++ public/js/classic.js
@@ -83,7 +83,7 @@ document.addEventListener("DOMContentLoaded", function () {
   let userInput;
 
   let timer;
-  let timeLeft = 10 * 60; // 5 minutes in seconds
+  let timeLeft = 0.1 * 60; // 5 minutes in seconds
 
   function startTimer() {
     timer = setInterval(function () {
@@ -192,7 +192,7 @@ document.addEventListener("DOMContentLoaded", function () {
   
 
   function resetGame() {
-    timeLeft = 10 * 60;
+    timeLeft = 0.1 * 60;
     userInput = '';
     document.getElementById('sqlInput').value = '';
     document.getElementById('timer').textContent = '';
